cac858cec3acb17beb736726e7aaa516cc63d65d
Muhammed <muhammednabeel@live.co.uk>
2023-02-23 10:38:16 +0000

    adds final changes

--- controllers/errorHandlingControllers.js
+++ controllers/errorHandlingControllers.js
@@ -5,7 +5,6 @@ const handleWrongPathErrors = (request, response, next) => {
 const handle404Errors = (error, request, response, next) => {
   if (error.status === 404) {
     response.status(404).send({ msg: "id provided does not exist" });
-<<<<<<< HEAD
   } else {
     next(error);
   }
@@ -14,29 +13,13 @@ const handle404Errors = (error, request, response, next) => {
 const handle500Errors = (error, request, response, next) => {
   if (error.status === 500) {
     response.status(500).send({ msg: "There has been a server error!" });
-=======
->>>>>>> 5e35f7160bfbecb54d496077307a7896754b9925
   } else {
-    next(error);
-  }
-};
-
-<<<<<<< HEAD
-const handle400Errors = (error, request, response, next) => {
-  if (error.status === 400) {
-=======
-const handle500Errors = (error, request, response, next) => {
-  if (error.status === 500) {
-    response.status(500).send({ msg: "There has been a server error!" });
-  } else {
-    error;
     next(error);
   }
 };
 
 const handle400Errors = (error, request, response, next) => {
   if (error.status === 400 || error.code === "22P02") {
->>>>>>> 5e35f7160bfbecb54d496077307a7896754b9925
     response.status(400).send({ msg: "Bad request" });
   } else {
     next(error);
--- models/reviewsModel.js
+++ models/reviewsModel.js
@@ -20,24 +20,15 @@ const fetchReviews = () => {
 };
 
 const fetchReviewsById = (reviewId) => {
-<<<<<<< HEAD
   const changedToNumber = +reviewId;
   if (isNaN(changedToNumber)) {
-=======
-  const changeToNumber = +reviewId;
-  if (isNaN(changeToNumber)) {
->>>>>>> 5e35f7160bfbecb54d496077307a7896754b9925
     return Promise.reject({ status: 400 });
   } else {
     return db
       .query(`SELECT * FROM reviews WHERE review_id = $1;`, [reviewId])
       .then((response) => {
         const selectedReviewArr = response.rows;
-<<<<<<< HEAD
         if (!selectedReviewArr.length) {
-=======
-        if (selectedReviewArr.length === 0) {
->>>>>>> 5e35f7160bfbecb54d496077307a7896754b9925
           return Promise.reject({ status: 404 });
         } else {
           return selectedReviewArr[0];
