ba9756c26f655b487d7e5e081c2f5d5ae1b69ca7
Muhammed <muhammednabeel@live.co.uk>
2023-03-13 09:54:36 +0000

    enables cors on the back end

--- db/app.js
+++ db/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const {
   handle500Errors,
@@ -8,8 +9,9 @@ const {
 } = require("../controllers/errorHandlingControllers.js");
 
 const apiRouter = require("../routes/api-router.js");
-
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
